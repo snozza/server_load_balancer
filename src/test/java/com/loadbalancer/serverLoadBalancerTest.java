daeaa609507a919e588db4423a93907ad55b6746
Andrew Snead <andrewcsnead@gmail.com>
2015-03-09 15:00:37 +0000

    setup test suite

---
+++ src/test/java/com/loadbalancer/serverLoadBalancerTest.java
@@ -0,0 +1,12 @@
+package serverloadbalancer;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class serverLoadBalancerTest {
+
+  @Test
+  public void itCompiles() {
+    assertThat(true).isEqualTo(true);
+  }
+}
